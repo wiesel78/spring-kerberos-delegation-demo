10f2551e0628e0b78760a7913e13e015d988a1cc
Philip Ullrich <philip.ullrich@dataport.de>
2025-05-20 01:01:15 +0200

    feat: add kerby kdc and adapt clients

--- kdc/kerby/start.sh
+++ kdc/kerby/start.sh
@@ -2,3 +2,14 @@ cd /opt/kerby/kdc-dist
 sh ./bin/kdcinit.sh ./conf/ ./keytabs/
 sh ./bin/start-kdc.sh ./conf ./runtime/
 #tail -f /dev/null
+
+sleep 10
+echo "addprinc -pw Passw0rd123 user1@IAM.DEV" | sh ./bin/kadmin.sh conf/ -k keytabs/admin.keytab
+echo "addprinc -pw Passw0rd123 user2@IAM.DEV" | sh ./bin/kadmin.sh conf/ -k keytabs/admin.keytab
+echo "addprinc -pw Passw0rd123 user3@IAM.DEV" | sh ./bin/kadmin.sh conf/ -k keytabs/admin.keytab
+
+echo "addprinc -randkey HTTP/frontend.iam.dev@IAM.DEV" | sh ./bin/kadmin.sh conf/ -k keytabs/admin.keytab
+echo "addprinc -randkey HTTP/backend.iam.dev@IAM.DEV" | sh ./bin/kadmin.sh conf/ -k keytabs/admin.keytab
+
+echo "ktadd -k /opt/kerby/kdc-dist/keytabs/frontend.keytab HTTP/frontend.iam.dev@IAM.DEV" | sh ./bin/kadmin.sh conf/ -k keytabs/admin.keytab
+echo "ktadd -k /opt/kerby/kdc-dist/keytabs/backend.keytab HTTP/backend.iam.dev@IAM.DEV" | sh ./bin/kadmin.sh conf/ -k keytabs/admin.keytab
