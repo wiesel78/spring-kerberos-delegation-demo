e1c53aa8ef3f35114d2a12d1d5027a1cd585234e
Philip Ullrich <wurst78@gmail.com>
2025-12-04 00:03:23 +0100

    refactor: standalone ldap and better principal handling

--- kdc/mit/entrypoint.sh
+++ kdc/mit/entrypoint.sh
@@ -1,75 +1,68 @@
 #!/bin/bash
 
+echo "VERSION: 1"
+echo "LDAP_KRB_CONTAINER_DN: ${LDAP_KRB_CONTAINER_DN}"
+echo "LDAP_SERVER_URL: ${LDAP_SERVER_URL}"
+
+# write password file for access to the ldap server
 echo -n "${LDAP_ADMIN_PASSWORD}" > /tmp/ldap_admin_password
 chmod 600 /tmp/ldap_admin_password
-ldap_admin_password_hash=$(slappasswd -T /tmp/ldap_admin_password)
 
-echo "Starting LDAP service..."
-service slapd start
-
-# set admin password for ldap
-ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
-dn: olcDatabase={1}mdb,cn=config
-changetype: modify
-replace: olcRootPW
-olcRootPW: ${ldap_admin_password_hash}
-EOF
-
-# add kerberos schema to ldap
-curl https://raw.githubusercontent.com/krb5/krb5/refs/heads/master/src/plugins/kdb/ldap/libkdb_ldap/kerberos.schema -o /etc/ldap/schema/kerberos.schema
-ldap-schema-manager -i /etc/ldap/schema/kerberos.schema
-ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
-dn: olcDatabase={1}mdb,cn=config
-add: olcDbIndex
-olcDbIndex: krbPrincipalName eq,pres,sub
-EOF
-
-ldapadd -x -D ${LDAP_ADMIN_DN} -y /tmp/ldap_admin_password -H ldapi:/// <<EOF
-dn: ${LDAP_SERVICE_DN}
-cn: $(echo ${LDAP_SERVICE_DN} | sed -E "s/.*cn=([^,]+).*/\1/g")
-objectClass: simpleSecurityObject
-objectClass: organizationalRole
-userPassword: ${LDAP_SERVICE_PASSWORD}
-description: Kerberos KDC Service Account
-EOF
-
-ldapmodify -Y EXTERNAL -H ldapi:/// <<EOF
-dn: olcDatabase={1}mdb,cn=config
-changetype: modify
-add: olcAccess
-olcAccess: {0}to dn.subtree="${LDAP_KRB_CONTAINER_DN}"
-  by dn="${LDAP_SERVICE_DN}" write
-  by dn="${LDAP_ADMIN_DN}" write
-  by * none
-EOF
-
-kdb5_ldap_util -D ${LDAP_ADMIN_DN} -w ${ADMIN_PRINC_PASS} create -subtrees ${LDAP_KRB_CONTAINER_DN} -r ${REALM} -P "${ADMIN_PRINC_PASS}" -s -H ldapi:///
+# create the kerberos subtree in ldap
+kdb5_ldap_util -D ${LDAP_ADMIN_DN} -w ${ADMIN_PRINC_PASS} create -subtrees ${LDAP_KRB_CONTAINER_DN} -r ${REALM} -P "${ADMIN_PRINC_PASS}" -s -H "${LDAP_SERVER_URL}"
 
 mkdir -p ${KEYTABS_PATH}
 
+# add admin user
 kadmin.local -q "addprinc -pw ${ADMIN_PRINC_PASS} admin/admin@${REALM}"
-kadmin.local -q "addprinc -pw P@ssw0rd123 +preauth +forwardable +proxiable +ok_as_delegate user1@${REALM}"
-kadmin.local -q "addprinc -randkey +ok_to_auth_as_delegate +ok_as_delegate +preauth HTTP/prefrontend.${DOMAIN}@${REALM}"
-kadmin.local -q "ktadd -k ${KEYTABS_PATH}/prefrontend.keytab HTTP/prefrontend.${DOMAIN}@${REALM}"
-kadmin.local -q "addprinc -randkey +ok_to_auth_as_delegate +ok_as_delegate +preauth HTTP/frontend.${DOMAIN}@${REALM}"
-kadmin.local -q "ktadd -k ${KEYTABS_PATH}/frontend.keytab HTTP/frontend.${DOMAIN}@${REALM}"
-kadmin.local -q "addprinc -randkey +preauth +ok_as_delegate HTTP/backend.${DOMAIN}@${REALM}"
-kadmin.local -q "ktadd -k ${KEYTABS_PATH}/backend.keytab HTTP/backend.${DOMAIN}@${REALM}"
 
-ldapmodify -x -D ${LDAP_ADMIN_DN} -y /tmp/ldap_admin_password -H ldapi:/// <<EOF
-dn: krbPrincipalName=HTTP/prefrontend.${DOMAIN}@${REALM},cn=${REALM},${LDAP_KRB_CONTAINER_DN}
+# collect all properties in use to unset them in the loop
+PROPERTY_KEYS=$(cat /etc/provision/accounts.yml | \
+  yq -r '[.users[] | to_entries | map(.key)[]] | unique | map("unset \(.)") | join("\n")')
+
+# read users from the config file and add them to kerberos and ldap
+cat /etc/provision/accounts.yml | \
+  yq -r '.users[] | to_entries | map("export \(.key)=\(.value)") | join(" ")' | \
+  while read -r LINE; do
+    eval "$property_keys"
+    eval "$LINE"
+
+    kadmin.local -q "addprinc -pw ${password} +preauth +forwardable +proxiable +ok_as_delegate ${name}@${REALM}"
+  done
+
+echo "add services..."
+PROPERTY_KEYS=$(cat /etc/provision/accounts.yml | \
+  yq -r '[.services[] | to_entries | map(.key)[]] | unique | map("unset \(.)") | join("\n")')
+echo "possible keys to cleanup $PROPERTY_KEYS"
+
+cat /etc/provision/accounts.yml | \
+  yq -r '.services[] | to_entries | map("export \(.key)=\(if (.value | type) == "array" then "'"'"'\(.value)'"'"'" else .value end)") | join(" ")' | \
+  while read -r LINE; do
+    echo "property keys : $PROPERTY_KEYS"
+    eval "$PROPERTY_KEYS"
+    echo "LINE: $LINE"
+    eval "$LINE"
+
+    echo "addprinc"
+    kadmin.local -q "addprinc -randkey +ok_to_auth_as_delegate +ok_as_delegate +preauth ${name}@${REALM}"
+    echo "ktadd"
+    rm -f "${KEYTABS_PATH}/${keytab}"
+    kadmin.local -q "ktadd -k ${KEYTABS_PATH}/${keytab} ${name}@${REALM}"
+
+    echo "add delegation: $delegates"
+    if [[ $(echo "${delegates:-[]}" | jq 'length') > 0 ]]; then
+      echo "delegates will be set"
+      delegation_targets=$(echo "$delegates" | jq -r 'map("krbAllowedToDelegateTo: \(.)@'${REALM}'") | join("\n")')
+
+      echo "delegate targets: $delegation_targets"
+      ldapmodify -x -D ${LDAP_ADMIN_DN} -y /tmp/ldap_admin_password -H "${LDAP_SERVER_URL}" <<-EOF
+dn: krbPrincipalName=${name}@${REALM},cn=${REALM},${LDAP_KRB_CONTAINER_DN}
 changetype: modify
 add: krbAllowedToDelegateTo
-krbAllowedToDelegateTo: HTTP/frontend.${DOMAIN}@${REALM}
-krbAllowedToDelegateTo: HTTP/backend.${DOMAIN}@${REALM}
+${delegation_targets}
 EOF
-
-ldapmodify -x -D ${LDAP_ADMIN_DN} -y /tmp/ldap_admin_password -H ldapi:/// <<EOF
-dn: krbPrincipalName=HTTP/frontend.${DOMAIN}@${REALM},cn=${REALM},${LDAP_KRB_CONTAINER_DN}
-changetype: modify
-add: krbAllowedToDelegateTo
-krbAllowedToDelegateTo: HTTP/backend.${DOMAIN}@${REALM}
-EOF
+    fi
+  done
 
 set -e
 
